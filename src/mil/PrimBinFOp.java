b07a50cc8e95ebf5374c8c9fbd35598b51b949c2
Mark P. Jones <mpj@pdx.edu>
2018-09-22 10:03:34 -0700

    [bugfix] Code generation for binary operators on Flag values (PrimBinFOp) should generate code using an LLVM primitive, not a call to an external function.

--- src/mil/PrimBinFOp.java
+++ src/mil/PrimBinFOp.java
@@ -35,4 +35,39 @@ public abstract class PrimBinFOp extends Prim {
     MILProgram.report("constant folding for " + getId());
     return new Done(new Return(Flag.fromBool(op(n, m))));
   }
+
+  /**
+   * Generate code for a MIL PrimCall with the specified arguments in a context where the primitive
+   * is not expected to produce any results, but execution is expected to continue with the given
+   * code.
+   */
+  llvm.Code toLLVMPrimVoid(
+      LLVMMap lm, VarMap vm, TempSubst s, Atom[] args, boolean isTail, llvm.Code c) {
+    debug.Internal.error(id + " is not a void primitive");
+    return c;
+  }
+
+  /**
+   * Generate code for a MIL PrimCall with the specified arguments in a context where the primitive
+   * is expected to return a result (that should be captured in the specified lhs), and then
+   * execution is expected to continue on to the specified code, c.
+   */
+  llvm.Code toLLVMPrimCont(
+      LLVMMap lm,
+      VarMap vm,
+      TempSubst s,
+      Atom[] args,
+      boolean isTail,
+      llvm.Local lhs,
+      llvm.Code c) {
+    return new llvm.Op(
+        lhs,
+        this.op(llvm.Type.i1, args[0].toLLVMAtom(lm, vm, s), args[1].toLLVMAtom(lm, vm, s)),
+        c);
+  }
+
+  /**
+   * Generate an LLVM right hand side for this binary MIL primitive with the given values as input.
+   */
+  abstract llvm.Rhs op(llvm.Type ty, llvm.Value l, llvm.Value r);
 }
