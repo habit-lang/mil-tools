d199b3c4eb6f439a2044b5d46d9b1ca4d4876d1f
Mark P. Jones <mpj@pdx.edu>
2018-10-17 20:25:32 -0700

    hello, world.

--- demo/runtime.c
+++ demo/runtime.c
@@ -2,9 +2,13 @@
 
 // A trivial run time library
 
-/* Provide an implementation for the printWord primitive.
- */
+/* Provide an implementation for the printWord primitive.  */
 void printWord(int x) {
   printf("%d\n", x);
+}
+
+/* Provide an implementation for the printString primitive.  */
+void printString(char* s) {
+  puts(s);
 }
 
